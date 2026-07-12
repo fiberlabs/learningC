e097f53c98509de6254dd78b9c570a0bc835a402
fiberlabs <fiberlabs@protonmail.com>
2026-07-12 17:32:15 -0400

    Properly Completed Project 19: Memory Buffer Playground.

--- 04-strings/projects/project19-memory_buffer_playground/memory.c
+++ 04-strings/projects/project19-memory_buffer_playground/memory.c
@@ -3,32 +3,47 @@
 
 int main()
 {
-    int arr1[5] = {10, 20, 30, 40, 50};
-    int arr2[5];
+    //part 1: integer arrays
+    int numbers1[5] = {1, 2, 3, 4, 5};
+    int numbers2[5];
 
-    memcpy(arr2, arr1, sizeof(arr1));
+    memcpy(numbers2, numbers1, sizeof(numbers1));
 
-    for (int i = 0; i < sizeof(arr1) / sizeof(arr1[0]); i++)
+    printf("\nInteger Array Number 1\n");
+    for (size_t i = 0; i < sizeof(numbers1) / sizeof(numbers1[0]); i++)
     {
-        printf("byte %d value: %d\n", i, arr2[i]);
+        printf("value %zu: %d\n", i, numbers1[i]);
     }
 
-    if (memcmp(arr1, arr2, sizeof(arr1)) == 0)
+    printf("\nInteger Array Number 2\n");
+    for (size_t i = 0; i < sizeof(numbers2) / sizeof(numbers2[0]); i++)
     {
-        printf("both arrays are the same\n");
-    }
-    else
-    {
-        printf("both arrays are different\n");
+        printf("value %zu: %d\n", i, numbers2[i]);
     }
 
-    memset(arr1, 0, sizeof(arr1));
-    for (int i = 0; i < sizeof(arr1) / sizeof(arr1[0]); i++)
-    {
-        printf("byte %d value: %d\n", i, arr1[i]);
+    if (memcmp(numbers1, numbers2, sizeof(numbers1)) != 0) {
+        printf("they are not identitcal\n");
+    }
+    else {
+        printf("they are identical\n");
     }
 
-    char text[] = "HELLO";
-    memmove(text, text + 1, strlen(text) + 1);
-    printf("%s\n", text);
+    //part 2: reset memory
+    memset(numbers2, 0, sizeof(numbers2));
+
+    printf("\nReset Memory Array 2\n");
+    for (size_t i = 0; i < sizeof(numbers2) / sizeof(numbers2[0]); i++)
+    {
+        printf("value %zu: %d\n", i, numbers2[i]);
+    }
+
+    //part 3: character array
+    char text[] = "SYSTEMS";
+    printf("orignal: %s\n", text);
+
+    memmove(text, text + 1, sizeof(text) - 1);
+    printf("modified: %s\n", text);
+
+
+    return 0;
 }
