78e5f409af63d2feef5b23f6f016cd070646fe35
fiberlabs <fiberlabs@protonmail.com>
2026-07-14 02:20:10 -0400

    Completed Updated Project 21: Input Sanitizer.

--- 04-strings/projects/project21-input_sanitizer/sanitizer.c
+++ 04-strings/projects/project21-input_sanitizer/sanitizer.c
@@ -2,36 +2,35 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_INPUT 100
-
 int main()
 {
-    char buffer[MAX_INPUT];
+    char buffer[100];
 
     // take user input
     printf("enter your password: ");
-
-    if (fgets(buffer, MAX_INPUT, stdin) == NULL)
-    {
-        printf("Input Error Occured :(\n");
-        return 1;
-    }
-
+    fgets(buffer, sizeof(buffer), stdin);
     buffer[strcspn(buffer, "\n")] = '\0';
 
-    for (int i = 0; buffer[i] != '\0'; i++)
+    // using strlen instead of sizeof because we want to know how many bytes the actual string is using
+    // not the total bytes in buffer[]
+    size_t length = strlen(buffer); //this is better for when we have to use larger strings
+    for (size_t i = 0; i < length; i++)
     {
-
-        // if something unsafe was found
-        if (buffer[i] == '|' || buffer[i] == ';' || buffer[i] == '&')
+        // first check for any punctuation characters in the string
+        if (ispunct(buffer[i]) != 0)
         {
-            printf("UNSAFE CHARACTER DETECTED!: %c... ending program\n", buffer[i]);
-            return 1;
+            // if it is a punct character, then check for any dangerous input
+            // you dont want to reject every punct char, only the dangerous ones
+            if (buffer[i] == ';' || buffer[i] == '|' || buffer[i] == '&')
+            {
+                printf("Potential command injection detected.... exiting program\n");
+                return 1;
+            }
         }
     }
 
-    // if nothing unsafe was found it will do this
-    printf("the user input is safe!\n");
+    // if nothing bad was detected
+    printf("you entered: %s\n", buffer);
 
     return 0;
 }
