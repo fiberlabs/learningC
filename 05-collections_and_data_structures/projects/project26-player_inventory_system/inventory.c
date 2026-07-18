0582d1cc77445f0ef7bf5175f0454fa0235a1357
fiberlabs <fiberlabs@protonmail.com>
2026-07-18 04:45:04 -0400

    Project 26 Update: Done List Items Logic.

--- 05-collections_and_data_structures/projects/project26-player_inventory_system/inventory.c
+++ 05-collections_and_data_structures/projects/project26-player_inventory_system/inventory.c
@@ -7,79 +7,94 @@ int main()
     int inventory[CAP] = {10, 20, 30, 0, 0};
     int count = 3;
 
-    // print menu and take user input
-    int menu_input = 100; // dummy val
-
-    while (menu_input != 1 && menu_input != 2 && menu_input != 3 && menu_input != 4)
+    while (1)
     {
-        printf("\n");
-        printf("========== MENU ==========\n");
-        printf("1. Add Item\n");
-        printf("2. Drop Last Item\n");
-        printf("3. Show Inventory\n");
-        printf("4. Exit Program\n");
-        printf("==========================\n");
+        // print menu and take user input
+        int menu_input = 100; // dummy val
 
-        printf("choose an option: ");
-        scanf("%d", &menu_input);
-
-        if (menu_input != 1 && menu_input != 2 && menu_input != 3 && menu_input != 4)
+        while (menu_input != 1 && menu_input != 2 && menu_input != 3 && menu_input != 4)
         {
-            printf("invalid input, try again\n");
-        }
-    }
+            printf("\n");
+            printf("========== MENU ==========\n");
+            printf("1. Add Item\n");
+            printf("2. Drop Last Item\n");
+            printf("3. Show Inventory\n");
+            printf("4. Exit Program\n");
+            printf("==========================\n");
 
-    // game logic
-    int add_input = 0;
-    switch (menu_input)
-    {
-    case 1:
-        // add item logic
+            printf("choose an option: ");
+            scanf("%d", &menu_input);
 
-        if (count < CAP) // bounds check
-        {
-            // print the existing items
-            for (int i = 0; i < count; i++)
+            if (menu_input != 1 && menu_input != 2 && menu_input != 3 && menu_input != 4)
             {
-                printf("SLOT %d: %d\n", i, inventory[i]);
-            }
-            // ask the user to enter an item ID
-            printf("enter an item ID: ");
-            scanf("%d", &add_input);
-
-            // add it to the array
-            inventory[count] = add_input;
-            count++;
-
-            //remove this after testing
-            printf("\nTEST PRINT\n");
-            for (int i = 0; i < count; i++)
-            {
-                printf("SLOT %d: %d\n", i, inventory[i]);
+                printf("invalid input, try again\n");
             }
         }
-        else
+
+        // game logic
+        int add_input = 0;
+        switch (menu_input)
         {
-            printf("inventory is full...\n");
+        case 1:
+            // add item logic
+
+            if (count < CAP) // bounds check
+            {
+                // print the existing items
+                for (int i = 0; i < count; i++)
+                {
+                    printf("SLOT %d: %d\n", i, inventory[i]);
+                }
+                // ask the user to enter an item ID
+                printf("enter an item ID: ");
+                scanf("%d", &add_input);
+
+                // add it to the array
+                inventory[count] = add_input;
+                count++;
+
+                // remove this after testing
+                printf("\nTEST PRINT\n");
+                for (int i = 0; i < count; i++)
+                {
+                    printf("SLOT %d: %d\n", i, inventory[i]);
+                }
+            }
+            else
+            {
+                printf("inventory is full...\n");
+            }
+
+            break;
+
+        case 2:
+            printf("drop item logic\n");
+            break;
+
+        case 3:
+            // show inventory logic
+            if (count > 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    printf("SLOT %d: %d\n");
+                }
+            }
+            else
+            {
+                printf("inventory is empty\n");
+            }
+
+            break;
+
+        case 4:
+            printf("exit program logic\n");
+            break;
+
+        default:
+            printf("invalid input or something went wrong\n");
+            break;
         }
-
-        break;
-
-    case 2:
-        printf("drop item logic\n");
-        break;
-
-    case 3:
-        printf("show inventory logic\n");
-        break;
-
-    case 4:
-        printf("exit program logic\n");
-        break;
-
-    default:
-        printf("invalid input or something went wrong\n");
-        break;
     }
 
     return 0;
