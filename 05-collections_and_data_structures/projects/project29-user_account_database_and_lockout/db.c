3ae9d17aa39c7fe72a2fdd34f8aa891f62f86416
fiberlabs <fiberlabs@protonmail.com>
2026-07-19 23:23:19 -0400

    Started Project 29: User Database and Lockout.

---
+++ 05-collections_and_data_structures/projects/project29-user_account_database_and_lockout/db.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#define CAPACITY 3
+
+struct User
+{
+    int id;
+    int pin;
+    int locked; //bool
+    int failures; //tally
+};
+
+
+int main()
+{
+    struct User users[CAPACITY];
+    int count = 0;
+
+    //manually creating users
+    
+
+    return 0;
+}
