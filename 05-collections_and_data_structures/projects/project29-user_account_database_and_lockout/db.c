77eb05bc59b73fc859859bd086bc578248c388c8
fiberlabs <fiberlabs@protonmail.com>
2026-07-19 23:33:59 -0400

    UPDATE Project 29: just need to save before i write the rest

--- 05-collections_and_data_structures/projects/project29-user_account_database_and_lockout/db.c
+++ 05-collections_and_data_structures/projects/project29-user_account_database_and_lockout/db.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 #define CAPACITY 3
 
@@ -18,7 +17,47 @@ int main()
     int count = 0;
 
     //manually creating users
-    
+    users[0].id = 100;
+    users[0].pin = 1234;
+    users[0].locked = 0;
+    users[0].failures = 0;
+    count++;
+
+    users[0].id = 101;
+    users[0].pin = 5678;
+    users[0].locked = 0;
+    users[0].failures = 0;
+    count++;
+
+    users[0].id = 102;
+    users[0].pin = 1011;
+    users[0].locked = 0;
+    users[0].failures = 0;
+    count++;
+
+    //ask the user for their ID
+    int user_input;
+    printf("enter your user id: ");
+    scanf("%d", &user_input);
+
+    //see if ID is in the array
+    int found = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (users[i].id == user_input)
+        {
+            found = 1;
+            break;
+        }
+    }
+
+    if (found == 0) { //if user ID was not found
+        printf("no existing user by that ID...\n");
+    }
+    else { //if the user id was found now do this
+
+    }
+
 
     return 0;
 }
