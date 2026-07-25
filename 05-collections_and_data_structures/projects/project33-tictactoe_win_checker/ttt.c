240257b3dd01dc2567c1d6af28e7aec9d7b3f0a6
fiberlabs <fiberlabs@protonmail.com>
2026-07-25 23:47:41 +0500

    UPDATE Project 33: Added Printing the Menu and The Win Conditions.

--- 05-collections_and_data_structures/projects/project33-tictactoe_win_checker/ttt.c
+++ 05-collections_and_data_structures/projects/project33-tictactoe_win_checker/ttt.c
@@ -25,7 +25,53 @@ int main()
     printf("\n");
 
     /*win conditions*/
-    
+    // checking columns
+    for (int j = 0; j < 3 && !winner_found; j++)
+    {
+        if (board[0][j] != '.' &&
+            board[0][j] == board[1][j] &&
+            board[1][j] == board[2][j])
+        {
+            winner = board[0][j];
+            winner_found = 1;
+        }
+    }
+
+    // checking rows
+    for (int i = 0; i < 3 && !winner_found; i++)
+    {
+        if (board[i][0] != '.' &&
+            board[i][0] == board[i][1] &&
+            board[i][1] == board[i][2])
+        {
+            winner = board[i][0];
+            winner_found = 1;
+        }
+    }
+
+    // checking diagonal 1
+    if (!winner_found)
+    {
+        if (board[0][0] != '.' &&
+            board[0][0] == board[1][1] &&
+            board[1][1] == board[2][2])
+        {
+            winner = board[0][0];
+            winner_found = 1;
+        }
+    }
+
+    // checking diagonal 2
+    if (!winner_found)
+    {
+        if (board[0][2] != '.' &&
+            board[0][2] == board[1][1] &&
+            board[1][1] == board[2][0])
+        {
+            winner = board[0][2];
+            winner_found = 1;
+        }
+    }
 
     return 0;
 }
