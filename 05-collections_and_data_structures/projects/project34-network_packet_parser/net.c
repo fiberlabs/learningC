100d1d83c9e093ec746171264dd8ab7b6db4cd4c
fiberlabs <fiberlabs@protonmail.com>
2026-07-26 23:48:42 +0500

    Started Project 34.

---
+++ 05-collections_and_data_structures/projects/project34-network_packet_parser/net.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+enum PacketType {
+    INTEGER_PAYLOAD,
+    FLOAT_PAYLOAD,
+    CHARACTER_PAYLOAD
+};
+
+union PacketData {
+    int int_data;
+    float float_data;
+    char text_data[10];
+
+};
+
+struct Packet{
+    int id;
+    enum PacketType packet_type;
+    union PacketData packet_data;
+};
+
+#define CAPACITY 3
+
+int main()
+{
+    struct Packet packets[CAPACITY];
+
+    //intializing them
+    packets[0].id = 100;
+    packets[0].packet_type = INTEGER_PAYLOAD;
+    packets[0].packet_data.int_data = 404;
+
+    packets[1].id = 101;
+    packets[1].packet_type = FLOAT_PAYLOAD;
+    packets[1].packet_data.float_data = 3.14;
+
+    packets[2].id = 102;
+    packets[2].packet_type = CHARACTER_PAYLOAD;
+    strcpy(packets[2].packet_data.text_data,"OK!");
+
+    return 0;
+}
