5cc4b28c90be7bd2e1d758fe4b7f657f85a6697e
fiberlabs <fiberlabs@protonmail.com>
2026-07-28 20:47:50 +0500

    Project 36: Added Menu and Drink Potion Logic for the Monster Room.

--- 06-functions/projects/project36-dungeon_explorer_with_functions/dungeon.c
+++ 06-functions/projects/project36-dungeon_explorer_with_functions/dungeon.c
@@ -12,18 +12,28 @@ enum CharacterStatus
 struct Player
 {
     int health;
-    int attack_dmg;
+    int attack_dmg; // this wull be randomly generated every turn
     int potions;
     int gold;
     enum CharacterStatus status;
 };
 
+struct Monster
+{
+    int health;
+    int attack_dmg; // this will be randomly generated every turn
+};
+
+// function signatures
 void start_menu();
 void start_game();
 int room_chance();
 void treasure_room(struct Player *user);
 void user_rest(struct Player *user);
 void trap_room(struct Player *user);
+void monster_room(struct Player *user, struct Monster *monster);
+int monster_room_menu(struct Player *user, struct Monster *monster);
+void drink_potion(struct Player *user);
 
 int main()
 {
@@ -85,7 +95,11 @@ void start_game()
 
         if (scenario <= 4)
         {
-            printf("monster room\n");
+            // create the monster
+            struct Monster monster;
+            monster.health = 5 + rand() % 11;
+
+            monster_room(&user, &monster);
         }
         else if (scenario <= 7)
         {
@@ -100,7 +114,8 @@ void start_game()
             printf("you found a empty room\n");
         }
 
-        if (user.status == DEAD) {
+        if (user.status == DEAD)
+        {
             printf("game over\n");
             return;
         }
@@ -169,7 +184,8 @@ void user_rest(struct Player *user)
             else
             {
                 user->health += 5;
-                if (user->health > 50) {
+                if (user->health > 50)
+                {
                     user->health = 50;
                 }
                 printf("you now have %d health\n", user->health);
@@ -179,16 +195,106 @@ void user_rest(struct Player *user)
     }
 }
 
-void trap_room(struct Player *user) {
+void trap_room(struct Player *user)
+{
     printf("OH NO! you fell into a trap room! :(\n");
     int hp_lost = 5 + rand() % 11;
     user->health -= hp_lost;
 
-    if (user->health <= 0) {
+    if (user->health <= 0)
+    {
         user->status = DEAD;
     }
-    else {
+    else
+    {
         printf("you took %d damage\n", hp_lost);
         printf("you have %d HP left...\n", user->health);
+    }
+}
+
+void monster_room(struct Player *user, struct Monster *monster) //test it out now
+{
+    int user_input = 0;
+
+    while (1)
+    {
+        int user_input = monster_room_menu(user, monster);
+
+        // if they want to drink a potion
+        if (user_input == 2)
+        {
+            drink_potion(user);
+        }
+        else {
+            printf("still have to write this logic\n");
+        }
+    }
+}
+
+int monster_room_menu(struct Player *user, struct Monster *monster)
+{
+    printf("entering roo...A MONSTER HAS APPEARED!\n");
+
+    printf("your HP:   \t%d\n", user->health);
+    printf("Potions:   \t%d\n", user->potions);
+    printf("Monster HP:\t%d\n", monster->health);
+
+    // take monster menu input
+    char monster_menu_buffer[10];
+    int mm_valid_input = 0; // mm means monster menu
+    while (1)
+    {
+        printf("'1' for attack and '2' for potion\n");
+
+        printf("do you want to attack or drink potion(+10HP)?: ");
+        fgets(monster_menu_buffer, sizeof(monster_menu_buffer), stdin);
+        monster_menu_buffer[strcspn(monster_menu_buffer, "\n")] = '\0';
+
+        if (strcmp(monster_menu_buffer, "1") != 0 && strcmp(monster_menu_buffer, "2") != 0)
+        {
+            printf("invalid input, try again\n");
+            continue;
+        }
+        else
+        {
+            mm_valid_input = 1;
+        }
+
+        if (mm_valid_input)
+        {
+            return atoi(monster_menu_buffer);
+        }
+    }
+}
+
+void drink_potion(struct Player *user)
+{
+    if (user->health >= 50)
+    {
+        printf("your health is already full\n");
+        return;
+    }
+    else
+    {
+        if (user->potions > 0)
+        {
+            user->potions--;
+            user->health += 10;
+
+            if (user->health > 50)
+            {
+                user->health = 50;
+                printf("using potion... you now have %d health", user->health);
+                return;
+            }
+
+            printf("using potion... you now have %d health", user->health);
+            return;
+        }
+        else
+        {
+            printf("you dont have any potions left\n");
+            return;
+        }
     }
 }
