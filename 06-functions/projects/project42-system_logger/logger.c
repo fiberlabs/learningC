bf0280b4fecb175b3c030412836d72974ba622b7
fiberlabs <fiberlabs@protonmail.com>
2026-08-01 11:27:37 +0500

    Completed Project 42: System Logger.

---
+++ 06-functions/projects/project42-system_logger/logger.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+enum Severity
+{
+    INFO,
+    WARNING,
+    ERROR
+};
+
+void print_log(enum Severity severity, ...)
+{
+    const char *levels[] =
+    {
+        "INFO",
+        "WARNING",
+        "ERROR"
+    };
+
+    va_list args;
+
+    va_start(args, severity);
+
+    const char *log = va_arg(args, const char *);
+
+    printf("[%s] %s", levels[severity], log);
+
+    va_end(args);
+}
+
+int main(void)
+{
+    print_log(0, "Successful Log In.\n");
+    print_log(1, "Disk usage at %d%%.\n", 85);
+    print_log(2, "Failed to open file 'data.bin'.\n");
+    return 0;
+}
