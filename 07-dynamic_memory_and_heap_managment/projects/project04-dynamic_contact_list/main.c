083ba87ec3e8e013d28df62af5598f358cfd2c49
fiberlabs <fiberlabs@protonmail.com>
2026-08-10 23:55:14 +0500

    Update Project 04: I am Lost.

--- 07-dynamic_memory_and_heap_managment/projects/project04-dynamic_contact_list/main.c
+++ 07-dynamic_memory_and_heap_managment/projects/project04-dynamic_contact_list/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "utils.h"
 
 int main() {
+    struct Details my_contact = fill_details();
+    
+    printf("\n--- Contact Summary ---\n");
+    printf("Phone: %s\n", my_contact.phone_number);
+    printf("Email: %s\n", my_contact.email_address);
 
+    // TODO: Remember to free memory later!
     return 0;
 }
--- 07-dynamic_memory_and_heap_managment/projects/project04-dynamic_contact_list/utils.h
+++ 07-dynamic_memory_and_heap_managment/projects/project04-dynamic_contact_list/utils.h
@@ -1,12 +1,43 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 struct Details {
     char *phone_number;
     char *email_address;
+};
+
+enum AllowedEmailServices
+{
+    GMAIL = 1,
+    HOTMAIL,
+    PROTON
+};
+
+struct EmailDetails
+{
+    int bytes_used_for_username;
+    char *username_storage;
+    char *domain_storage;
+    enum AllowedEmailServices service;
+    char *full_email;
 };
 
 struct PhoneNumberDetails {
     int number_of_bytes;
 };
 
-struct Details fill_details();
+struct Details fill_details(void);
 
 char *fill_phone_number(struct Details *created_struct_parameter);
+
+char *fill_email_address(void); // Matches parameterless definition
+
+int email_menu(void);
+
+char *get_domain_string(enum AllowedEmailServices service);
+
+char *get_username(struct EmailDetails *username_parameter);
+
+char *get_domain(struct EmailDetails *email_parameter);
+
+#endif
