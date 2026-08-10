2444ef22e69c3b3b9554148f4bb3b99126537b5d
fiberlabs <fiberlabs@protonmail.com>
2026-08-11 02:27:10 +0500

    Update Project 4: Added Logic for Name.

--- 07-dynamic_memory_and_heap_managment/projects/project04-dynamic_contact_list/main.c
+++ 07-dynamic_memory_and_heap_managment/projects/project04-dynamic_contact_list/main.c
@@ -1,13 +1,85 @@
 #include <stdio.h>
-#include "utils.h"
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    struct Details my_contact = fill_details();
-    
-    printf("\n--- Contact Summary ---\n");
-    printf("Phone: %s\n", my_contact.phone_number);
-    printf("Email: %s\n", my_contact.email_address);
+int how_many_contacts(int *number_of_contacts_para)
+{
+    int user_input = 0;
+    while (user_input <= 0)
+    {
+        printf("how many contacts do you want to add?: ");
+        if (scanf("%d", &user_input) == 1) {
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+        }
 
-    // TODO: Remember to free memory later!
+    }
+
+    *number_of_contacts_para = user_input;
+    return *number_of_contacts_para;
+}
+
+char *get_contact_name()
+{
+    int capacity = 8;
+    char *name_buffer = malloc(capacity * sizeof(char));
+
+    printf("enter contact's name: ");
+    int bytes_used = 0;
+    int index = 0;
+    int user_input;
+    while ((user_input = getchar()) != '\n' && user_input != EOF)
+    {
+        bytes_used++;
+
+        while (1)
+        {
+            if (bytes_used >= capacity - 1)
+            {
+                char *temp = realloc(name_buffer, (capacity * 2) * sizeof(char));
+                if (temp == NULL) {
+                    printf("error in realloc for name_buffer\n");
+                    free(name_buffer);
+                    name_buffer = NULL;
+                    return NULL;
+                }
+
+                capacity *= 2;
+                name_buffer = temp;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        name_buffer[index] = user_input;
+        index++;
+    }
+
+    name_buffer[bytes_used] = '\0';
+
+    return name_buffer;
+}
+
+int main()
+{
+    int number_of_contacts = how_many_contacts(&number_of_contacts);
+
+    char ***contacts = malloc(number_of_contacts * sizeof(char **));
+    if (contacts == NULL)
+    {
+        printf("error in malloc for 'contacts'\n");
+        return 1;
+    }
+
+    for (int i = 0; i < number_of_contacts; i++)
+    {
+        contacts[i] = malloc(3 * sizeof(char *));
+        // fill in the name
+        contacts[i][0] = get_contact_name();
+        printf("contact %d name: %s\n", i, contacts[i][0]);
+    }
+
     return 0;
 }
--- 07-dynamic_memory_and_heap_managment/projects/project04-dynamic_contact_list/test/test_utils.c
+++ 07-dynamic_memory_and_heap_managment/projects/project04-dynamic_contact_list/test/test_utils.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "utils.h"
+#include "test_utils.h"
 
 int email_menu()
 {
