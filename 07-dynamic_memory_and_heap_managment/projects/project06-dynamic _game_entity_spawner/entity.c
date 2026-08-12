667e1d523b3fb018915c8bfb65fda7067bb2043d
fiberlabs <fiberlabs@protonmail.com>
2026-08-12 13:33:51 +0500

    Started Project 06: Dynamic Game Entity Spawner.

---
+++ 07-dynamic_memory_and_heap_managment/projects/project06-dynamic _game_entity_spawner/entity.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct Enemy {
+    char *name;
+    int cordinate_x;
+    int cordinate_y;
+};
+
+struct Enemy *spawn_enemy(char *name_parameter, int x_parameter, int y_parameter);
+
+int main() {
+    
+    return 0;
+}
