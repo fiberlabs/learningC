b0c2123302c038b5b87e7e1d202018c9a78d7030
fiberlabs <fiberlabs@protonmail.com>
2026-08-21 00:41:15 +0500

    Update Project 07: Added Logic for getting the quest's name.

--- 07-dynamic_memory_and_heap_managment/projects/project07-quest_log_system/quest.c
+++ 07-dynamic_memory_and_heap_managment/projects/project07-quest_log_system/quest.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 struct Quest {
-    char *title_name;
+    char *quest_name;
     struct Quest *next;
 };
 
@@ -18,7 +18,6 @@ int menu_input() {
             while((c = getchar()) != '\n' && c != EOF);
         }
 
-
         if ((user_input != 1 && user_input != 2 && user_input != 3)) {
             printf("invalid input, try again\n> ");
         }
@@ -27,8 +26,39 @@ int menu_input() {
     return user_input;
 }
 
-int main() {
+char *get_name() {
+    int bytes_used = 0;
+    int byte_index = 0;
 
+    int bytes_capacity = 8;
+    char *name_buffer = malloc(bytes_capacity * sizeof(char));
+    if (name_buffer == NULL) {printf("error: malloc name_buffer"); return NULL;}
+
+    printf("enter quest name: ");
+    int name_input;
+    while ((name_input = getchar()) != '\n' && name_input != EOF)
+    {
+        bytes_used++;
+
+        if (bytes_used > bytes_capacity - 1) {
+            char *temp = realloc(name_buffer, (bytes_capacity * 2) * sizeof(char));
+            if (temp == NULL) {printf("error: realloc temp"); free(name_buffer); name_buffer = NULL; return NULL;}
+            name_buffer = temp;
+            free(temp); //cuz we no longer need it
+        }
+
+        bytes_capacity *= 2;
+        name_buffer[byte_index] = name_input;
+        byte_index++;
+    }
+    
+    name_buffer[bytes_used] = '\0'; //adding the null byte
+
+    return name_buffer;
+}
+
+int main() {
+    printf("entered name: %s\n", get_name());
 
     return 0;
 }
