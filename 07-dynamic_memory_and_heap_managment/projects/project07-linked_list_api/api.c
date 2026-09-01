88397612f9c2699e8a67a368417d06fb5d9a7c55
fiberlabs <fiberlabs@protonmail.com>
2026-09-02 04:29:46 +0500

    started Project 07: Linked List API.

---
+++ 07-dynamic_memory_and_heap_managment/projects/project07-linked_list_api/api.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int id;
+    struct Node *next;
+};
+
+static struct Node *first_node = NULL;
+
+struct Node *add_node(int id)
+{
+    struct Node *new_node = malloc(sizeof(struct Node));
+    if (new_node == NULL)
+    {
+        printf("error: malloc new_node");
+        return NULL;
+    }
+
+    new_node->id = id;
+    new_node->next = NULL;
+
+    if (first_node == NULL) {
+        first_node = new_node;
+        return new_node;
+    }
+
+    struct Node *current_node = NULL;
+    current_node = first_node;
+    while (current_node->next != NULL) //as long as ->next != NULL
+    {
+        current_node = current_node->next; //keep moving the finger onwards
+    }
+    current_node->next = new_node; //once you reach NULL, append the new node
+    //creating the chain
+    
+
+    return new_node;
+}
+
+int main()
+{
+    add_node(10);
+    add_node(20);
+    add_node(30);
+
+    return 0;
+}
