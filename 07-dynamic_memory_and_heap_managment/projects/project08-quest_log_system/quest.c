4a7359976a2c62f6a41202dd5cf876c736b1e87e
fiberlabs <fiberlabs@protonmail.com>
2026-09-02 12:33:56 +0500

    Update Project 08: added logic for add_quest().

--- 07-dynamic_memory_and_heap_managment/projects/project08-quest_log_system/quest.c
+++ 07-dynamic_memory_and_heap_managment/projects/project08-quest_log_system/quest.c
@@ -8,6 +8,33 @@ struct Quest
     struct Quest *next;
 };
 
+static struct Quest *first_quest = NULL;
+
+struct Quest *add_quest(char *name)
+{
+    struct Quest *new_quest = malloc(sizeof(struct Quest));
+    if (new_quest == NULL)
+    {
+        printf("error: malloc for new_quest");
+        return NULL;
+    }
+
+    strcpy(new_quest->title, name);
+    new_quest->next = NULL;
+
+    if (first_quest == NULL) {
+        first_quest = new_quest;
+        return new_quest;
+    }
+
+    struct Quest *current_quest = NULL;
+    current_quest = first_quest;
+    while (current_quest->next != NULL) {
+        current_quest = current_quest->next;
+    }
+    current_quest->next = new_quest;
+}
+
 int main()
 {
 
