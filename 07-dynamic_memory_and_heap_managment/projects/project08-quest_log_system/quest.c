03254ec72462c1aa9a4d57bf8d371935d92c4fd1
fiberlabs <fiberlabs@protonmail.com>
2026-09-02 12:49:37 +0500

    Completed Project 08: Quest Log System.

--- 07-dynamic_memory_and_heap_managment/projects/project08-quest_log_system/quest.c
+++ 07-dynamic_memory_and_heap_managment/projects/project08-quest_log_system/quest.c
@@ -22,14 +22,16 @@ struct Quest *add_quest(char *name)
     strcpy(new_quest->title, name);
     new_quest->next = NULL;
 
-    if (first_quest == NULL) {
+    if (first_quest == NULL)
+    {
         first_quest = new_quest;
         return new_quest;
     }
 
     struct Quest *current_quest = NULL;
     current_quest = first_quest;
-    while (current_quest->next != NULL) {
+    while (current_quest->next != NULL)
+    {
         current_quest = current_quest->next;
     }
     current_quest->next = new_quest;
@@ -37,26 +39,49 @@ struct Quest *add_quest(char *name)
     return new_quest;
 }
 
-void print_quests() {
-    if (first_quest == NULL) {
+void print_quests()
+{
+    if (first_quest == NULL)
+    {
         printf("there are no quests to print\n");
         return;
     }
 
     struct Quest *current_quest = NULL;
     current_quest = first_quest;
-    while(current_quest != NULL) {
+    while (current_quest != NULL)
+    {
         printf("Title: %s\nMemory Address: %p\n\n", current_quest->title, (void *)current_quest);
         current_quest = current_quest->next;
     }
+}
 
+void destroy_quests()
+{
+    if (first_quest == NULL)
+    {
+        printf("there are no quests to destroy\n");
+        return;
+    }
+
+    struct Quest *current_quest = NULL;
+    current_quest = first_quest;
+    while (current_quest != NULL)
+    {
+        struct Quest *temp = current_quest->next;
+        free(current_quest);
+        current_quest = temp;
+    }
+    first_quest = NULL;
 }
 
 int main()
 {
-    print_quests();
     add_quest("slay the dragon");
+    add_quest("become elden lord");
+    add_quest("leave central yharnam");
     print_quests();
+    destroy_quests();
 
     return 0;
 }
